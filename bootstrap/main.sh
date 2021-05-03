548f22d2c9dc87f2ca1c71c4c3f20848b55faaac
mediahomes <akmalharith@gmx.com>
2021-05-03 04:26:39 +0000

    Update from https://github.com/mediahomes/media-scraper/commit/b1c06219e005c04663fef28caeab6222cfb71722

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Login to ghcr.io and pull the latest epg-grabber
 # echo $PAT | docker login ghcr.io -u $GHCR_USER --password-stdin
@@ -7,21 +7,21 @@ docker pull ghcr.io/mediahomes/epg-grabber:latest
 
 # Execute grabber and check if it succeeds
 RUN1=$(docker run -d -e EPG_CONFIG=my -e EPG_DAYS=7 ghcr.io/mediahomes/epg-grabber)
-if ! docker top $RUN1 &>/dev/null
-then
-    echo "Container crashed unexpectedly."
-    return 1
-fi
 
 # Wait before running another container
 sleep 60
 
 RUN2$=$(docker run -d -e EPG_CONFIG=premium -e EPG_DAYS=7 ghcr.io/mediahomes/epg-grabber)
-if ! docker top $RUN2 &>/dev/null
-then
-    echo "Container crashed unexpectedly."
-    return 1
-fi
+
+# Check if containers are running succesfully
+docker ps -aq | while read line
+do
+	if ! docker top $line &>/dev/null
+    then
+        echo "Container" $line "crashed unexpectedly."
+        exit 1
+    fi
+done
 
 # Wait until execution completes
 until [[ -z $(docker ps -q) ]]
