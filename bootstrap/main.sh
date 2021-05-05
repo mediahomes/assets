9b0aff1438a7bf3f1e6c7b41ccfd8d37aa631943
bertperrisor <22504259+bertperrisor@users.noreply.github.com>
2021-05-05 18:55:52 +0800

    Update main.sh

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -8,8 +8,23 @@ docker pull ghcr.io/mediahomes/epg-grabber:latest
 # Execute grabber and check if it succeeds
 docker run -d -e EPG_CONFIG=my -e EPG_DAYS=7 ghcr.io/mediahomes/epg-grabber
 
+docker ps -aq | while read line
+do
+	if ! docker top $line &>/dev/null
+	then
+        	echo "Container" $line "crashed unexpectedly."
+        exit 1
+    fi
+done
+
+# Wait until execution completes
+until [[ -z $(docker ps -q) ]]
+do
+  sleep 3
+done
+
 # Wait before running another container
-sleep 60
+# sleep 60
 
 docker run -d -e EPG_CONFIG=premium -e EPG_DAYS=7 ghcr.io/mediahomes/epg-grabber
 
