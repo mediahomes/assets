248b546b45de24751aa2ccbae18e88b66d3edbf6
mediahomes <akmalharith@gmx.com>
2022-01-21 20:10:36 +0000

    Update from https://github.com/mediahomes/private/commit/16452615e725432e40d8bc79f06e188ee37ac9c3

--- 1/bootstrap/root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Download the latest bootstrap main.sh
-MAIN_BOOTSTRAP="https://mediahomes.github.io/epg-config-files/bootstrap/main.sh"
-
-# Execute bootstrap
-curl MAIN_BOOTSTRAP -o $HOME/main.sh
